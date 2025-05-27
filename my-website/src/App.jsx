f114ac1f4e6daa976f4aa75b1ace668c575eea54
IAustinBehrensI <behrens.austin16@gmail.com>
2025-05-26 21:00:40 -0400

    formatting

--- my-website/src/App.jsx
+++ my-website/src/App.jsx
@@ -1,9 +1,53 @@
-function App(){
+function App() {
   return (
-    <div>
-      <h1>Austin Behrens</h1>
-    </div>
+    <div className="fullscreen">
+      <div className="center-name">
+        <h1>Austin Behrens</h1>
+      </div>
+        <div className="title-block">
+          <p>Software & data developer</p>
+          <p>Versatile</p>
+          <p>Project-driven</p>
+          <p>Solutions-oriented</p>
+        </div>
+
+        <div className="about-block">
+          <p>[ ] About me</p>
+        </div>
+          <div className="link-block">
+          <p>Links</p>
+          <div className="linkI">[ ] LinkedIn</div>
+          <div className="linkG">[ ] GitHub</div>
+          <div className="linkR">[ ] Resume</div>
+          <p>Contact</p>
+          <div className = "linkE">[ ]Behrens.austin16@gmail.com</div> 
+          <div className = "linkP">[ ]+1 516-880-3197</div>
+        </div></div>
   );
 }
 
-export default App
+export default App;
+
+
+
+
+/*
+ctrl c in terminal to end
+
+1. Run your dev server to work locally 
+cd my-website
+npm run dev
+
+#Once ready to test to pubic web page
+
+# 2. Commit & push your code to GitHub
+git add .cd
+git commit -m "Describe your changes"
+git push origin main
+
+# 3. Deploy the latest build to GitHub Pages
+npm run deploy
+
+link to website https://IAustinBehrensI.github.io/
+
+*/
